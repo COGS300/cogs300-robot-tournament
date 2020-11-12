f445c19b8d777ebce99e5f83a23502385c811c0a
Jason <jasondien@gmail.com>
2020-11-12 14:33:04 -0800

    refactor agent script to CogsAgent and MyAgent; visibility refactoring; dynamic object variable and material instantiation

---
+++ cogs300-final-project/Assets/Scripts/CogsAgent.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Random = UnityEngine.Random;
+
+public class CogsAgent : Agent
+{
+    // ------------------------VARIABLES----------------------------
+    // public: direct access available for all classes
+    // protected: direct access restricted to CogsAgent and MyAgent
+    // private: direct access restricted to CogsAgent only;
+    //          indirect access via Getters and Setters if they are provided
+
+    protected Rigidbody rBody;
+    protected GameObject timer, enemy, myBase;
+    protected Transform baseLocation;  
+    
+    protected GameObject[] targets;
+    public List<GameObject> capturedTargets;
+    protected List<GameObject> carriedTargets;
+
+    protected Vector3 dirToGo, rotateDir;
+
+    private const float maxMoveSpeed = 1;
+    private const float maxTurnSpeed = 150;
+    private int team;
+
+    private const float m_LaserLength = 20;
+    private GameObject myLaser;
+    private bool m_Shoot, frozen;
+    private float frozenTime, moveSpeed, turnSpeed;
+
+    protected Dictionary<string, float> rewardDict;
+
+
+
+    // ----------------AGENT FUNCTIONS-----------------
+   
+    public override void OnEpisodeBegin()
+    {
+        rBody.velocity = Vector3.zero;
+
+        if (team == 1)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, -125f, 0f));
+        }
+        else if (team == 2)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 50f, 0f));
+        }
+        capturedTargets.Clear();
+        carriedTargets.Clear();
+        frozen = false;
+
+        transform.localPosition = new Vector3(baseLocation.localPosition.x, 0.5f, baseLocation.localPosition.z);
+        // foreach (GameObject target in targets)
+        // {
+        //     target.GetComponent<Target>().resetGame();
+        // }
+        //myBase.GetComponent<HomeBase>().Reset();
+
+    }
+
+
+
+    // -----------------BASIC SETUP--------------------
+    
+    private void Freeze() {
+        if (rewardDict.ContainsKey("frozen")) AddReward(rewardDict["frozen"]);
+        frozen = true;
+        frozenTime = Time.time;
+    }
+    private void DropCarrying() {
+        for (int i = carriedTargets.Count - 1; i > -1; i--)
+            {
+                GameObject currentTarget = carriedTargets[i];
+                currentTarget.GetComponent<Target>().Carry(0);
+                currentTarget.GetComponent<Target>().setMass(1);
+                currentTarget.GetComponent<Target>().explode();
+                carriedTargets.Remove(currentTarget);
+
+            }
+    }
+
+
+
+    // ---------------VIRTUAL FUNCTIONS-----------------
+    // Used for setup as well as in MyAgent
+    
+    protected virtual void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        targets = GameObject.FindGameObjectsWithTag("Target");
+
+        timer = GameObject.FindGameObjectWithTag("Timer");
+        
+        GameObject[] agents = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject agent in agents) {
+            if (agent != gameObject) {
+                enemy = agent;
+            }
+        }
+        
+        if (gameObject.name == "Agent 1") {
+            team = 1;
+        }
+        else team = 2;
+
+        myBase = GameObject.Find("Base " + team);
+        baseLocation = myBase.transform;
+
+        myLaser = GameObject.Find("Laser " + team);
+
+        dirToGo = Vector3.zero;
+        rotateDir = Vector3.zero;
+        capturedTargets = new List<GameObject>();
+        carriedTargets = new List<GameObject>();
+
+        Material mat;
+        if (team == 1) {
+            mat = (Material) Resources.Load<Material>(WorldConstants.agent1ID + "/AgentMat"); 
+        }
+        else {
+            mat = (Material) Resources.Load<Material>(WorldConstants.agent2ID + "/AgentMat"); 
+        }
+        gameObject.GetComponent<Renderer>().material = mat;
+    }
+    
+    protected virtual void FixedUpdate() {
+        moveSpeed = maxMoveSpeed - (0.05f * carriedTargets.Count);
+        turnSpeed = maxTurnSpeed - (10f * carriedTargets.Count);
+
+
+        if (rBody.velocity.sqrMagnitude > 25f) // slow it down
+        {
+            float maxVelocity = 0.95f - 0.03f * carriedTargets.Count;
+            if (maxVelocity <= 0.6f) { maxVelocity = 0.6f; }
+            rBody.velocity *= maxVelocity;
+
+        }
+
+        if (Time.time > frozenTime + 4f && frozen)
+        {
+            frozen = false;
+        }
+    }
+    
+    protected virtual void OnTriggerEnter(Collider collision)
+    {
+        //If I collide with a target which is not in my own base
+        
+        if (collision.gameObject.CompareTag("HomeBase") && collision.gameObject.GetComponent<HomeBase>().team == team)
+        {
+            for (int i = carriedTargets.Count - 1; i > -1; i--)
+            {
+                GameObject currentTarget = carriedTargets[i];
+                //AddReward(0.1f);
+                capturedTargets.Add(currentTarget);
+                int spot = myBase.GetComponent<HomeBase>().addToFirstSpotInBase();
+                Vector3 position = myBase.GetComponent<HomeBase>().getPosition(spot);
+                currentTarget.GetComponent<Target>().addToBase(spot, team, position);
+                currentTarget.GetComponent<Target>().zeroRotation();
+                carriedTargets.Remove(currentTarget);
+
+            }
+            collision.gameObject.GetComponent<HomeBase>().numInBase = capturedTargets.Count;
+        }
+    }
+
+    // check collisions for walls and add a small negative reward
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target") && collision.gameObject.GetComponent<Target>().inBase != team && collision.gameObject.GetComponent<Target>().carried == 0 && !frozen)
+        {
+            //SetReward(0.5f);
+            //carrying++;
+            collision.gameObject.GetComponent<Target>().setMass(0);
+            collision.gameObject.GetComponent<Target>().Carry(team);
+            collision.gameObject.GetComponent<Target>().zeroRotation();
+
+            
+            carriedTargets.Add(collision.gameObject);
+
+            //if it is in my enemies base (not my base, and in a base)
+            // if (collision.gameObject.GetComponent<Target>().inBase != 0)
+            // {
+            //     foreach (GameObject target in targets)
+            //     {
+            //         if (target.GetComponent<Target>().inBase != 0 && target.GetComponent<Target>().inBase != team)
+            //         {
+            //             target.GetComponent<Target>().adjustSpotInBase();
+            //         }
+            //     }
+            // }
+        }
+        
+    }
+
+
+    
+    // ---------------USEFUL HELPERS------------------
+    // can be used in MyAgent
+    
+    // return true if laser succesfully activated, false otherwise
+    protected bool LaserControl() {
+        if (IsLaserOn() && !IsFrozen())
+        {
+            //AddReward(-0.05f);
+            if (rewardDict.ContainsKey("shooting-laser")) AddReward(rewardDict["shooting-laser"]);
+            var myTransform = transform;
+            myLaser.transform.localPosition = new Vector3(0f,0f,(m_LaserLength / 2f) + 0.5f);
+            myLaser.transform.localScale = new Vector3(1f, 1f, m_LaserLength);
+            var rayDir = 25.0f * myTransform.forward;
+            Debug.DrawRay(myTransform.position, rayDir, Color.red, 0f, true);
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, 2f, rayDir, out hit, m_LaserLength))
+            {
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    if (rewardDict.ContainsKey("hit-enemy")) AddReward(rewardDict["hit-enemy"]);
+                    hit.collider.gameObject.GetComponent<CogsAgent>().DropCarrying();
+                    hit.collider.gameObject.GetComponent<CogsAgent>().Freeze();
+                }
+            }
+            return true;
+        }
+        else
+        {
+            myLaser.transform.localScale = new Vector3(0f, 0f, 0f);
+            myLaser.transform.localPosition = new Vector3(0f,0f,0f);
+
+            return false;
+        }
+    }
+
+    protected GameObject getNearestTarget(){
+        float distance = 200;
+        GameObject nearestTarget = null;
+        foreach (var target in targets)
+        {
+            float currentDistance = Vector3.Distance(target.transform.localPosition, transform.localPosition);
+            if (currentDistance < distance && target.GetComponent<Target>().carried == 0 && target.GetComponent<Target>().inBase != team){
+                distance = currentDistance;
+                nearestTarget = target;
+            }
+        }
+        return nearestTarget;
+    }
+
+
+
+    // --------------GETTERS----------------
+    public int GetTeam() {return team;}     
+    protected float GetMoveSpeed() {return moveSpeed;}
+    protected float GetTurnSpeed() {return turnSpeed;}
+    public bool IsLaserOn() {return m_Shoot;}
+    public bool IsFrozen() {return frozen;}
+    public float GetFrozenTime() {return frozenTime;}
+    public int GetCaptured() {return capturedTargets.Count;}
+    public int GetCarrying() {return carriedTargets.Count;}
+    public float DistanceToBase(){return Vector3.Distance(myBase.transform.localPosition, transform.localPosition);}
+
+    // --------------SETTERS----------------
+    protected void SetLaser(bool on) {m_Shoot = on;}
+    
+}
---
+++ cogs300-final-project/Assets/Scripts/Setup/FinalStageManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FinalStageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isTraining = false;
+    public Text winnerTextbox;
+    public GameObject timer;
+
+    public GameObject agent1;
+    public GameObject agent2;
+    public GameObject base1;
+    public GameObject base2;
+    public Text base1CountTxt;
+    public Text base2CountTxt;
+
+
+    GameObject[] targets;
+    void Start()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Target");
+
+        winnerTextbox.enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        bool timerIsRunning = timer.GetComponent<Timer>().GetTimerIsRunning();
+        
+        int base1Num = agent1.GetComponent<MyAgent>().GetCaptured();
+        int base2Num = agent2.GetComponent<MyAgent>().GetCaptured();
+        int agent1Carry = agent1.GetComponent<MyAgent>().GetCarrying();
+        int agent2Carry = agent2.GetComponent<MyAgent>().GetCarrying();
+        float agent1BaseDist = agent1.GetComponent<MyAgent>().DistanceToBase();
+        float agent2BaseDist = agent1.GetComponent<MyAgent>().DistanceToBase();
+
+        base1CountTxt.text = WorldConstants.agent1ID + ": " + base1Num.ToString();
+        base2CountTxt.text = WorldConstants.agent2ID + ": " + base2Num.ToString();
+     
+        if (!timerIsRunning)
+        {
+            if (base1Num > base2Num)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(1f);
+                agent2.GetComponent<MyAgent>().SetReward(-1f);
+                Debug.Log("Agent 1 wins by capture");
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 1 wins";
+            }
+            
+            else if (base2Num > base1Num)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(-1f);
+                agent2.GetComponent<MyAgent>().SetReward(1f);
+                Debug.Log("Agent 2 wins by capture");                
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 2 wins";
+            }
+            else if (agent1Carry > agent2Carry)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(1f);
+                agent2.GetComponent<MyAgent>().SetReward(-1f);
+                Debug.Log("Agent 1 wins by carry");
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 1 wins";
+            }
+            
+            else if (agent2Carry > agent1Carry)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(-1f);
+                agent2.GetComponent<MyAgent>().SetReward(1f);
+                Debug.Log("Agent 2 wins by carry");                
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 2 wins";
+            }
+            else if (agent1BaseDist < agent2BaseDist && agent1Carry != 0)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(1f);
+                agent2.GetComponent<MyAgent>().SetReward(-1f);
+                Debug.Log("Agent 1 wins by distance");
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 1 wins";
+            }
+            
+            else if (agent2BaseDist < agent1BaseDist && agent2Carry != 0)
+            {
+                agent1.GetComponent<MyAgent>().SetReward(-1f);
+                agent2.GetComponent<MyAgent>().SetReward(1f);
+                Debug.Log("Agent 2 wins by distance");                
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Agent 2 wins";
+            }
+            
+            else {
+                agent1.GetComponent<MyAgent>().SetReward(0f);
+                agent2.GetComponent<MyAgent>().SetReward(0f);
+                Debug.Log("Draw!");
+
+                winnerTextbox.enabled = true;
+                winnerTextbox.text = "Draw";
+            }
+
+            if (isTraining) {
+                Reset();
+            }
+            else {
+                StopGame();
+            }
+            
+        }
+    }
+
+    void Reset() {
+        timer.GetComponent<Timer>().Reset();
+        base1.GetComponent<HomeBase>().Reset();
+        foreach (GameObject target in targets)
+        {
+            target.GetComponent<Target>().resetGame();
+        }
+        
+        agent1.GetComponent<MyAgent>().EndEpisode();
+        agent2.GetComponent<MyAgent>().EndEpisode();
+    }
+
+    void StopGame() {
+        Time.timeScale = 0;
+    }
+}
